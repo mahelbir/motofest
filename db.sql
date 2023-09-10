a897596baa42a6f62c57c9461d9970b4ebc917da
Mahmuthan Elbir <me@mahmuthanelbir.com.tr>
2023-09-10 18:51:24 +0300

    sql eklendi

---
+++ db.sql
@@ -0,0 +1,55 @@
+--
+-- Tablo için tablo yapısı `siparisler`
+--
+
+CREATE TABLE `siparisler` (
+  `id` int(11) NOT NULL,
+  `icerik` text NOT NULL,
+  `tutar` decimal(10,2) NOT NULL,
+  `tarih` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `urunler`
+--
+
+CREATE TABLE `urunler` (
+  `id` int(11) NOT NULL,
+  `isim` varchar(255) NOT NULL,
+  `fiyat` decimal(10,2) NOT NULL,
+  `resim` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dökümü yapılmış tablolar için indeksler
+--
+
+--
+-- Tablo için indeksler `siparisler`
+--
+ALTER TABLE `siparisler`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `urunler`
+--
+ALTER TABLE `urunler`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Dökümü yapılmış tablolar için AUTO_INCREMENT değeri
+--
+
+--
+-- Tablo için AUTO_INCREMENT değeri `siparisler`
+--
+ALTER TABLE `siparisler`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `urunler`
+--
+ALTER TABLE `urunler`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
